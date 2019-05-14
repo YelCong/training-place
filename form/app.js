2a3959b0f2882ee800139d4334b1b53eec8c8418
yecihong <yecihong@4399inc.com>
2019-05-14 11:50:41 +0800

    form

---
+++ form/app.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        ipt1: 'message1',
+        txt1: "111\n1111\n",
+        // checked: false,
+        checked: 'this f',
+        checkedNames: ['Mike'],
+        picked: true,
+        selected:"",
+        ms:[],
+        sitems: [
+            { text: 'One', value: 'A' },
+            { text: 'Two', value: 'B' },
+            { text: 'Three', value: 'C' }
+        ],
+        v1:'oneeeeee',
+        v2:'twoooo'
+    },
+    methods: {},
+    computed: {}
+});
