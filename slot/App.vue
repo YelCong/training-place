01e8bd7cf925222f46ddda6d9dcb7ff82fe1f4a3
yecihong <yecihong@4399inc.com>
2019-05-23 10:57:09 +0800

    slot

---
+++ slot/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <app-header></app-header>
+</template>
+
+<script>
+    import Header from './components/header.vue';
+
+    export default {
+        name: "app",
+        components:{
+            'app-header': Header,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ slot/components/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>{{title}}</header>
+</template>
+
+<script>
+    export default {
+        name: "header",
+        data() {
+            return {
+                title: '组件组合&solt'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
